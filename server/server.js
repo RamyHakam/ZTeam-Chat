7d576eae558f57577ffee341619d12a6a133f455
Ramy Hakam <ramyhakam2@gmail.com>
2017-06-21 20:35:24 +0200

    heroku config

--- server/server.js
+++ server/server.js
@@ -1,14 +1,13 @@
 const path =require('path');
 const express =require('express');
 const masterpath= path.join(__dirname,'../public');
-
+const port = process.port ||3000;
 //creating app 
 
 
 var app= express();
 app.use(express.static(masterpath));
-app.listen(3000,()=>{
-console.log("server is running ");
+app.listen(port,()=>{
+console.log(`server running on port ${port}`);
 })
 
-console.log(masterpath);
