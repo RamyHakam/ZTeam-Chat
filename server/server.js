8aa153a28f4a8c023832ca1e9979c3a629fd3a18
Ramy Hakam <ramyhakam2@gmail.com>
2017-06-23 20:05:59 +0200

    Adding web socket connection

--- server/server.js
+++ server/server.js
@@ -1,13 +1,28 @@
 const path =require('path');
 const express =require('express');
+const socketio=require('socket.io');
+const http=require('http');
 const masterpath= path.join(__dirname,'../public');
 const port = process.env.PORT || 3000;
 //creating app 
-
-
 var app= express();
+//creating app with http 
+
+var server= http.createServer(app);
+//adding socketio
+var io= socketio(server);
 app.use(express.static(masterpath));
-app.listen(port,()=>{
+
+io.on('connection',(socket)=>{
+console.log("new user connection");
+socket.on('disconnect',()=>{
+console.log("disconnected");
+
+
+});
+});
+
+server.listen(port,()=>{
 console.log(`server running on port ${port}`);
 })
 
