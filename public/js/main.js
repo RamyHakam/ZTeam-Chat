b816c4c7be3b8d8ef917c86b9c48422b6d2d1aa8
Ramy Hakam <ramyhakam2@gmail.com>
2017-06-26 16:50:01 +0200

    send and recive from ui

--- public/js/main.js
+++ public/js/main.js
@@ -12,8 +12,45 @@ console.log("connection closed");
 
 socket.on('newMessage',function(message){
 console.log("new message",message);
+var dt=new Date($.now());
+var time = dt.getHours() + ":" + dt.getMinutes() + ":" + dt.getSeconds();
 
+var data =` <li class="right clearfix"><span class="chat-img pull-right">
+                            <img src="../img/avatar5.png"  style=" width:50px;" alt="User Avatar" class="img-circle" />
+                        </span>
+                            <div class="chat-body clearfix">
+                                <div class="header">
+                                    <small class=" text-muted"><span class="glyphicon glyphicon-time"> ${time}</span></small>
+                                    <strong class="pull-right primary-font">${message.from}</strong>
+                                </div>
+                                <p>
+                                  ${message.text}.
+                                </p>
+                            </div>
+                        </li>
+                        `;
+
+
+
+
+
+
+
+var li= jQuery(data);
+//li.text(`${message.from}:${message.text}`);
+jQuery("#messages").append(li);
 
 });
+
+ jQuery('#message-form').on('submit',function(e){
+e.preventDefault();
+socket.emit('createMessage',{
+from:'user',
+text:jQuery('#message').val()
+});
+ });
+
+
+
 
 //socket.emit("createMessage",{to:'ramy',from:'test'});
