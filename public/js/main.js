26d55b04e1222ddac8916c753af367ec7bf3e788
Ramy Hakam <ramyhakam2@gmail.com>
2017-06-27 15:55:51 +0200

    adding join page

--- public/js/main.js
+++ public/js/main.js
@@ -12,33 +12,65 @@ console.log("connection closed");
 
 socket.on('newMessage',function(message){
 console.log("new message",message);
+
+
+//scroll to bottom
+
+function scrollToBottom(){
+
+
+var it= jQuery('#testit');
+var message= jQuery('#messages');
+
+var newMessage= message.children('li:last-child')
+var clientH= it.prop('clientHeight');
+var scrollTop=it.prop('scrollTop');
+var scrollH=it.prop('scrollHeight');
+var newMessageH= newMessage.innerHeight();
+var prevmessageH=newMessage.prev().innerHeight();
+var t=clientH+scrollTop+newMessageH+prevmessageH;
+if(clientH+scrollTop+newMessageH+prevmessageH>=scrollH){
+it.scrollTop(scrollH);
+}
+}
+
 var dt=new Date($.now());
 var time = dt.getHours() + ":" + dt.getMinutes() + ":" + dt.getSeconds();
 
-var data =` <li class="right clearfix"><span class="chat-img pull-right">
-                            <img src="../img/avatar5.png"  style=" width:50px;" alt="User Avatar" class="img-circle" />
-                        </span>
-                            <div class="chat-body clearfix">
-                                <div class="header">
-                                    <small class=" text-muted"><span class="glyphicon glyphicon-time"> ${time}</span></small>
-                                    <strong class="pull-right primary-font">${message.from}</strong>
-                                </div>
-                                <p>
-                                  ${message.text}.
-                                </p>
-                            </div>
-                        </li>
-                        `;
+
+var formatesTime=moment(message.createdAt).format('hh:mm a');
+var template= jQuery('#message-template').html();
+var html= Mustache.render(template,{
+
+text:message.text,
+from:message.from,
+createdAt:formatesTime
+
+
+});
+jQuery("#messages").append(html);
+scrollToBottom();
 
 
 
+// var data =` <li class="right clearfix"><span class="chat-img pull-right">
+//                             <img src="../img/avatar5.png"  style=" width:50px;" alt="User Avatar" class="img-circle" />
+//                   // </span>
+//                             <div class="chat-body clearfix">
+//                                 <div class="header">
+//                                     <small class=" text-muted"><span class="glyphicon glyphicon-time"> ${time}</span></small>
+//                                     <strong class="pull-right primary-font">${message.from}</strong>
+//                                 </div>
+//                                 <p>
+//                                   ${message.text}.
+//                                 </p>
+//                             </div>
+//                         </li>
+//                         `;
 
-
-
-
-var li= jQuery(data);
+// var li= jQuery(data);
 //li.text(`${message.from}:${message.text}`);
-jQuery("#messages").append(li);
+//jQuery("#messages").append(li);
 
  var aSound = document.createElement('audio');
      aSound.setAttribute('src', '../img/sounds/sound.mp3');
@@ -52,6 +84,9 @@ socket.emit('createMessage',{
 from:'user',
 text:jQuery('#message').val()
 });
+text:jQuery('#message').val("");
+
+
  });
 
 
